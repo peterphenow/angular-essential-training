b9a906249ed879a97159d04501242f115bc7a651
Peter Phenow <peter.phenow@gmail.com>
2021-08-18 17:05:11 -0500

    setting up NgModule

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+
+@NgModule({
+  // imports is used to bring in other Angular modules that this module will need
+  imports: [BrowserModule],
+  // declarations is used to make components, directives, and pipes available to the module that don't come from another module.
+  declarations: [AppComponent],
+  // bootstrap is used for root module and let Angular know which component/s starting point for bootstrap process
+  // i.e. entry point for App code
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
