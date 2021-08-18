c7d96d987b8e82573926e8c5d5244f6f392d62e7
Peter Phenow <peter.phenow@gmail.com>
2021-08-18 17:58:45 -0500

    adding comments to better understand the bootstrap logic flow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,8 @@ import { Component } from "@angular/core";
 @Component({
   // selector is what Angular will use to locate a custom HTML element and apply the component to.
   // found in index.html
-  selector: "app-root",
+  // want to use a selector with a dash because it is a custom DOM element, WC3 says dash should be included in custom elements.
+  selector: "mw-app",
   template: "<h1>My App</h1>",
 })
 export class AppComponent {}
--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,6 @@
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { AppModule } from "./app/app.module";
 
+// Angular will first look for the AppModule component. This component has the bootstrap metadata which lists components to use as the starting component(AppComponent)
+// AppComponent is then displayed inside the matching selector listed in the metadata on the index.html file.
 platformBrowserDynamic().bootstrapModule(AppModule);
