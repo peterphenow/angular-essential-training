cbbcda68d7b308a2be803cfc9a058fa9624808c7
Peter Phenow <peter.phenow@gmail.com>
2021-08-19 10:34:12 -0500

    created media-item component and added styling

--- src/app/media-item.component.ts
+++ src/app/media-item.component.ts
@@ -1,8 +1,15 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: "mw-media-item",
   templateUrl: "./media-item.component.html",
   styleUrls: ["./media-item.component.css"],
 })
-export class MediaItemComponent {}
+export class MediaItemComponent {
+  // using the Input decorator
+  @Input() mediaItem;
+
+  onDelete() {
+    console.log("delete clicked!");
+  }
+}
